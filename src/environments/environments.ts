2f452651483dd0e329ead5f39c86e93a632ce55a
Chiziane <eddybruno43@gmail.com>
2026-01-12 22:48:40 +0200

    refactor(customer): clean and organize component code without changing functionality

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:8080',
+  baseURL4: 'http://localhost:8080',
   baseURL3: 'http://auth-service-java-spring-angular-production.up.railway.app',
   baseURL2: 'https://auth-service-java-spring-angular-1.onrender.com',
   baseURL1: 'https://carloadsystemmanagement.onrender.com',
-  baseURL4: 'https://watersytemmanagementjava-production.up.railway.app'
+  baseURL: 'https://watersytemmanagementjava-production.up.railway.app'
 };
